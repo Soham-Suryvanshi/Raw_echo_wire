33ca50c7fd20f733b465d9864b9fe76e8206af6f
Soham-Suryvanshi <soham.suryvanshi19@gmail.com>
2026-07-02 10:09:35 +0000

    Initial Commit

---
+++ day1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <cstdlib>
+#include<unistd.h>
+
+
+int main(){
+   int server_fd = socket(AF_INET , SOCK_STREAM , 0);
+   if(server_fd<0){
+    perror("socket failed");
+    exit(EXIT_FAILURE);
+   }
+   std::cout<<"SOCKET created,fd="<<server_fd<<std::endl;
+   int opt =1 ;
+   if(setsockopt(server_fd,SOL_SOCKET,SO_REUSEADDR,&opt,sizeof(opt))<0){
+    perror("setsocket failed");
+    exit(EXIT_FAILURE);
+   }
+    sockaddr_in address{};
+    address.sin_family=AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(6379);
+
+    if(bind(server_fd,(struct sockaddr*)&address,sizeof(address))<0){
+    perror("bind failed");
+    exit(EXIT_FAILURE);
+   }
+   std::cout<<"bound to 0.0.0.0:6379"<<std::endl;
+   if(listen(server_fd,10)<0){
+    perror("listen failed");
+    exit(EXIT_FAILURE);
+   }
+   std::cout<<"listening on port 6379..."<<std::endl;
+   std::cin.get();
+   return 0;
+}
